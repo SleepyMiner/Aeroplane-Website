5763d06c2e0e757ec6aa0a76b6df23720c91ae12
Sleepy_Miner <90234481+SleepyMiner@users.noreply.github.com>
2023-10-06 19:01:07 +0530

    fixed the scroll behaviour in hotelBooking

--- Build/php/hotelBookingPage.php
+++ Build/php/hotelBookingPage.php
@@ -98,20 +98,20 @@ include_once("functions.php");
                     <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Book a Restful Stay Easy as Flight Booking</h1>
                     <p class="mt-6 text-lg leading-8 text-gray-600">Enjoy effortless hotel reservations. Explore diverse options for a smooth and comfortable stay.</p>
                     <div class="mt-10 flex items-center justify-center gap-x-6">
-                        <a href="#" onclick="scrollToBookingForm(event)" class="frounded-md bg-pink-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Book Now</a>
+                        <a href="#" onclick="scrollToBookingForm(event)" class="rounded-md bg-pink-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Book Now</a>
                     </div>
                 </div>
             </div>
             <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
-                <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-b from-pink-400 via-rose-200 to-red-200 opacity-50 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+                <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-b from-pink-400 via-rose-200 to-red-200 opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
             </div>
         </div>
     </div>
 
 
     <!----HOTEL BOOKING FORM--->
-    <div id="booking-form-section" class="w-2/3 mx-auto py-32 sm:py-48 lg:py-56" id="hotel-form">
-        <div class="relative block text-center mb-8">
+    <div class="w-2/3 mx-auto py-32 sm:py-48 lg:py-56" id="hotel-form">
+        <div class="relative block text-center mb-8" id="booking-form-section">
             <span class="mx-auto text-3xl font-bold">Hotel Reservation</span>
         </div>
 
